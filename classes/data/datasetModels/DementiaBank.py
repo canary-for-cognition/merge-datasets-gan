6ce852cbc0a1d036aa469d9e7825c506e15f9309
JoyceHe1998 <heyizhuo2020@gmail.com>
2021-12-01 11:34:08 -0800

    added dataset model for canary dataset

---
+++ classes/data/datasetModels/DementiaBank.py
@@ -0,0 +1,12 @@
+from typing import Tuple
+
+from torch.utils.data import Dataset
+
+
+class DementiaBank(Dataset):
+
+    def __init__(self, fname, modality='text'):
+        pass
+
+    def __getitem__(self, idx: int) -> Tuple:
+        pass
---
+++ classes/data/datasetModels/Ondri.py
@@ -0,0 +1,12 @@
+from typing import Tuple
+
+from torch.utils.data import Dataset
+
+
+class Ondri(Dataset):
+
+    def __init__(self, fname, modality="eye_tracking"):
+        pass
+
+    def __getitem__(self, idx: int) -> Tuple:
+        pass
